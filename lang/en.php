2a59d44540e069cfbbfe7ff92cd604e7e82d21f2
Emilien52 <emiliengarandeau@yahoo.fr>
2022-09-18 17:12:38 +0200

    Mise en place nouveau système de traduction

--- lang/en.php
+++ lang/en.php
@@ -1,44 +1,41 @@
 <?php
 
-/* Administration dashboard */
-const REDIRECT_DASHBOARD_TITLE = "Redirect - List";
-const REDIRECT_DASHBOARD_TITLE_ADD = "Redirect - Add a redirection";
-const REDIRECT_DASHBOARD_TITLE_EDIT = "Redirect - Edit a redirection";
-const REDIRECT_DASHBOARD_TITLE_STATS = "Redirect - Stats";
-const REDIRECT_DASHBOARD_DESC = "Manage your redirections";
-const REDIRECT_DASHBOARD_DESC_EDIT = "Edit a redirection";
-const REDIRECT_DASHBOARD_DESC_STATS = "Stats of your redirections";
-
-const REDIRECT_DASHBOARD_NAME = "Name";
-const REDIRECT_DASHBOARD_NAME_PLACEHOLDER = "Enter the name of your redirection";
-
-const REDIRECT_DASHBOARD_SLUG = "Slug";
-const REDIRECT_DASHBOARD_SLUG_PLACEHOLDER = "Enter the slug ( ex: discord )";
-const REDIRECT_DASHBOARD_SLUG_HINT = "The slug is the url the player get with the redirect";
-
-const REDIRECT_DASHBOARD_TARGET = "Target";
-const REDIRECT_DASHBOARD_TARGET_PLACEHOLDER = "Enter the url target ( ex: https://discord.gg/H2eNaZZ98b )";
-
-const REDIRECT_DASHBOARD_STATS_TITLE_CLICK = "Number of clicks per redirections";
-
-const REDIRECT_DASHBOARD_STATS_NUMBER = "Number of redirections";
-const REDIRECT_DASHBOARD_STATS_CLICKS_TOTAUX = "Number of total clicks";
-const REDIRECT_DASHBOARD_STATS_CLICKS_ACTIVES = "Number of total clicks active";
-
-
-const REDIRECT_LIST_TABLE_ID = "ID";
-const REDIRECT_LIST_TABLE_NAME = "Name";
-const REDIRECT_LIST_TABLE_SLUG = "Slug";
-const REDIRECT_LIST_TABLE_TARGET = "Target";
-const REDIRECT_LIST_TABLE_CLICK = "Clicks";
-const REDIRECT_LIST_TABLE_EDIT = "Editing";
-
-const REDIRECT_TOAST_TITLE_SUCCESS = "Information";
-const REDIRECT_TOAST_TITLE_ERROR = "Error";
-
-const REDIRECT_TOAST_CREATE_SUCCESS = "Redirection added";
-const REDIRECT_TOAST_CREATE_ERROR_NAME = "Name already use";
-const REDIRECT_TOAST_CREATE_ERROR_SLUG = "Slug already use";
-
-const REDIRECT_TOAST_EDIT_SUCCESS = "Editing successful";
-const REDIRECT_TOAST_DELETE_SUCCESS = "Deleting successful";
+return[
+	"dashboard" => [
+		"title" => "Redirect - List",
+		"title_add" => "Redirect - Add a redirection",
+		"title_edit" => "Redirect - Edit a redirection",
+		"title_stats" => "Redirect - Stats",
+		"desc" => "Manage your redirections",
+		"desc_edit" => "Edit a redirection",
+		"desc_stats" => "Stats of your redirections",
+		"name" => "Name",
+		"name_placeholder" => "Enter the name of your redirection",
+		"slug" => "Slug",
+		"slug_placeholder" => "Enter the slug ( ex: discord )",
+		"slug_hint" => "The slug is the url the player get with the redirect",
+		"target" => "Target",
+		"target_placeholder" => "Enter the url target ( ex: https://discord.gg/H2eNaZZ98b )",
+		"stats_title_click" => "Number of clicks per redirections",
+		"stats_number" => "Number of redirections",
+		"stats_clicks_totaux" => "Number of total clicks",
+		"stats_clicks_actives" => "Number of total clicks active",
+	],
+	"list_table" => [
+		"id" => "ID",
+		"name" => "Name",
+		"slug" => "Slug",
+		"target" => "Target",
+		"click" => "Clicks",
+		"Edit" => "Editing",
+	],
+	"toast" => [
+		"title_success" => "Information",
+		"title_error" => "Error",
+		"create_success" => "Redirection added",
+		"create_error_name" => "Name already use",
+		"create_error_slug" => "Slug already use",
+		"edit_success" => "Editing successful",
+		"delete_success" => "Deleting successful",
+	],
+];
