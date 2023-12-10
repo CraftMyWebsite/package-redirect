c0a8fe0af49778525b8c222a2b158fba7b8add17
Teyir <60007549+Teyir@users.noreply.github.com>
2023-12-10 12:16:27 +0100

    [IMPROVES] Update to new Package class configuration

---
+++ Package.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace CMW\Package\Redirect;
+
+use CMW\Manager\Package\IPackageConfig;
+use CMW\Manager\Package\PackageMenuType;
+use CMW\Manager\Package\PackageSubMenuType;
+
+class Package implements IPackageConfig
+{
+    public function name(): string
+    {
+        return "Redirect";
+    }
+
+    public function version(): string
+    {
+        return "0.0.1";
+    }
+
+    public function authors(): array
+    {
+        return ["Teyir"];
+    }
+
+    public function isGame(): bool
+    {
+        return false;
+    }
+
+    public function isCore(): bool
+    {
+        return false;
+    }
+
+    public function menus(): ?array
+    {
+        return [
+            new PackageMenuType(
+                lang: "fr",
+                icon: "fas fa-directions",
+                title: "Redirect",
+                url: null,
+                permission: null,
+                subMenus: [
+                    new PackageSubMenuType(
+                        title: 'Gestion',
+                        permission: 'redirect.show',
+                        url: 'redirect/list',
+                    ),
+                    new PackageSubMenuType(
+                        title: 'Statistiques',
+                        permission: 'redirect.stats',
+                        url: 'redirect/stats',
+                    ),
+                ]
+            ),
+            new PackageMenuType(
+                lang: "en",
+                icon: "fas fa-directions",
+                title: "Redirect",
+                url: null,
+                permission: null,
+                subMenus: [
+                    new PackageSubMenuType(
+                        title: 'Manage',
+                        permission: 'redirect.show',
+                        url: 'redirect/list',
+                    ),
+                    new PackageSubMenuType(
+                        title: 'Statistics',
+                        permission: 'redirect.stats',
+                        url: 'redirect/stats',
+                    ),
+                ]
+            ),
+        ];
+    }
+}
